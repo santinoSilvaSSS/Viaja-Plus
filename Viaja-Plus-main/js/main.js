1055c7acdd90208e9f0d1ad8f80ed3d8dc0b7c30
Maaupelozo <mpelozo723@gmail.com>
2024-09-19 00:20:07 -0300

    Add files via upload

---
+++ Viaja-Plus-main/js/main.js
@@ -0,0 +1,11 @@
+
+window.embeddedChatbotConfig = {
+chatbotId: "xRLlO66znwTdjE-6rMYsr",
+domain: "www.chatbase.co"
+}
+
+
+src="https://www.chatbase.co/embed.min.js"
+chatbotId="xRLlO66znwTdjE-6rMYsr"
+domain="www.chatbase.co"
+defer
